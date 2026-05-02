99d714760b5fd219246fad009a3c47758729d60b
alfarezyyd <adityaalfarezyd@gmail.com>
2026-05-02 14:46:30 +0700

    feat(gateway): delete existing main.go and create gateway with HTTP setup

---
+++ services/gateway/cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go-tokopaedi-microservices/services/gateway/injector"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"go.uber.org/fx"
+)
+
+func Run(fxLifecycle fx.Lifecycle, ginEngine *gin.Engine, viperConfig *viper.Viper) {
+	webPort := viperConfig.GetString("API_PORT")
+	fxLifecycle.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			logrus.Debug("Starting server...")
+			go func() {
+				if err := ginEngine.Run(fmt.Sprintf(":%s", webPort)); err != nil {
+					panic(err)
+				}
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			logrus.Debug("Stopping server...")
+			return nil
+		},
+	})
+}
+
+func main() {
+
+	fxContainer := fx.New(
+		// Provider
+		injector.CoreModule,
+		// Invoker
+		fx.Invoke(Run),
+	)
+
+	fxContainer.Run()
+}
