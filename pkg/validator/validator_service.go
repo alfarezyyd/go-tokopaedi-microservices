b508952973759425671029268c4e2b56d4404183
alfarezyyd <adityaalfarezyd@gmail.com>
2026-05-04 15:20:38 +0700

    feat(validator): Add validator

--- pkg/validator/validator_service.go
+++ pkg/validator/validator_service.go
@@ -1,10 +1,7 @@
 package validator
 
-import universalTranslator "github.com/go-playground/universal-translator"
-
 type Service interface {
 	ValidateStruct(targetValidatorStruct interface{}) error
 	ValidateVar(targetValidatorStruct interface{}, validatorTags string) error
-	ParseValidationError(validationError error)
-	GetEngTranslator() universalTranslator.Translator
+	ParseValidationError(validationError error, dtoStruct interface{})
 }
--- pkg/validator/validator_service_impl.go
+++ pkg/validator/validator_service_impl.go
@@ -3,6 +3,9 @@ package validator
 import (
 	"go-tokopaedi-microservices/pkg/exception"
 	"net/http"
+	"reflect"
+	"regexp"
+	"strings"
 
 	universalTranslator "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
@@ -32,16 +35,72 @@ func (validatorService *ServiceImpl) ValidateVar(target interface{}, validatorTa
 }
 
 // ParseValidationError - Parsing error validasi ke dalam format yang lebih mudah dibaca
-func (validatorService *ServiceImpl) ParseValidationError(validationError error) {
+func (validatorService *ServiceImpl) ParseValidationError(validationError error, dtoStruct interface{}) {
 	if validationError != nil {
 		parsedMap := make(map[string]interface{})
-		for _, fieldError := range validationError.(validator.ValidationErrors) {
-			parsedMap[fieldError.Field()] = fieldError.Translate(validatorService.engTranslator)
+		reflectType := reflect.TypeOf(dtoStruct)
+		if reflectType.Kind() == reflect.Ptr {
+			reflectType = reflectType.Elem()
 		}
-		panic(exception.NewApplicationError(http.StatusBadRequest, exception.ErrBadRequest))
+		for _, fieldError := range validationError.(validator.ValidationErrors) {
+			fieldJSON := getJSONFieldName(reflectType, fieldError.StructField())
+			fieldName := fieldError.StructField()
+			translatedMessage := fieldError.Translate(validatorService.engTranslator)
+
+			// Hapus nama field dari pesan error
+			cleanMessage := strings.Replace(translatedMessage, fieldName, getPropertyFieldName(reflectType, fieldName), 1)
+			cleanMessage = strings.TrimSpace(cleanMessage) // Hilangkan spasi berlebih
+
+			parsedMap[fieldJSON] = cleanMessage
+		}
+		panic(exception.NewApplicationErrorSpecific(http.StatusBadRequest, exception.StatusValidationError, exception.MsgValidationError, parsedMap))
 	}
 }
 
-func (validatorService *ServiceImpl) GetEngTranslator() universalTranslator.Translator {
-	return validatorService.engTranslator
+// getPropertyFieldName mengembalikan nama field berdasarkan tag "property" atau memisahkan nama struct field berdasarkan huruf besar.
+func getPropertyFieldName(t reflect.Type, fieldName string) string {
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+
+		if field.Name == fieldName {
+			// Cek apakah ada tag "property"
+			propertyTag := field.Tag.Get("property")
+			if propertyTag != "" {
+				return propertyTag
+			}
+
+			// Jika tidak ada, pisahkan berdasarkan huruf besar (CamelCase → "Camel Case")
+			return splitCamelCase(field.Name)
+		}
+	}
+	return fieldName
+}
+
+// splitCamelCase memisahkan nama field yang dalam format CamelCase menjadi kata-kata terpisah.
+func splitCamelCase(s string) string {
+	re := regexp.MustCompile(`([a-z])([A-Z])`)
+	return re.ReplaceAllString(s, "$1 $2")
+}
+
+func getJSONFieldName(structType reflect.Type, fieldName string) string {
+	if structType.Kind() == reflect.Ptr {
+		structType = structType.Elem()
+	}
+
+	field, found := structType.FieldByName(fieldName)
+	if !found {
+		return fieldName // fallback ke nama field struct
+	}
+
+	jsonTag := field.Tag.Get("json")
+	if jsonTag == "" {
+		jsonTag = field.Tag.Get("form")
+		if jsonTag == "" {
+			return fieldName
+		}
+	}
+
+	// json tag bisa seperti "username,omitempty", ambil sebelum koma
+	parts := strings.Split(jsonTag, ",")
+	return parts[0]
 }
